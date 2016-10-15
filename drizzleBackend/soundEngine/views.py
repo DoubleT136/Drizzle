6c8b48713c3dbae9c018c4450da33e64e52fd3ef
ctang <tommy9695@gmail.com>
2016-10-15 04:25:08 -0400

    added drizzle backend

---
+++ drizzleBackend/soundEngine/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+def index(request):
+    if request.method == 'POST':
+        if request.is_ajax():
+            email = request.POST.get('hi')
+            data = {"hi":email}
+            return JsonResponse(data)
+    #return render(request,'index.html')
--- test_mp3.py
+++
@@ -1,9 +0,0 @@
-import build_mp3
-
-cmshort = add_to_audio(AudioSegment.silent(10000), "cm.mp3", 0, 1)
-eotwshort = add_to_audio(AudioSegment.silent(10000), "endoftheworld.mp3", 0, 1)
-ayhshort = add_to_audio(AudioSegment.silent(10000), "areyouhappy.mp3", 0, 1)
-
-track = cmshort + eotwshort + ayhshort
-
-track.export("comp.mp3", format="mp3")
